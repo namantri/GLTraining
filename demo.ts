cb0410c4a07affae2ca49898fb0c166327b7e0ce
Naman Tripathi <naman.tripathi@globallogic.com>
2023-02-22 11:18:54 +0530

    Second Commit

---
+++ demo.ts
@@ -0,0 +1,9 @@
+let firstName: string = "Naman";
+console.log(firstName);
+var num1: number = 13;
+var num2: number = 38;
+console.log(num1+num2);    
+let data: string|number; // This is called composite data types in this we can give two types to a variable
+data = 23;
+data = "abcd";
+console.log(data);
